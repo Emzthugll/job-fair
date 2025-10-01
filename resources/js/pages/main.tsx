b3e4b8695419e262ae1eebec519ad209a61ad01c
Emzthugll <marcjairus0111@gmail.com>
2025-10-01 17:02:37 +0800

    success message

--- resources/js/pages/main.tsx
+++ resources/js/pages/main.tsx
@@ -2,7 +2,9 @@ import JobPreference from '@/components/tabs/JobPreference';
 import PersonalInfoForm from '@/components/tabs/PersonalInfoForm';
 import WorkExperience from '@/components/tabs/WorkExperience';
 import { Applicant } from '@/types/Applicant';
-import { Form, usePage } from '@inertiajs/react';
+import { Form } from '@inertiajs/react';
+import { QrCode } from 'lucide-react';
+import { useState } from 'react';
 import StepWizard from 'react-step-wizard';
 
 interface MainProps {
@@ -13,32 +15,51 @@ interface MainProps {
 }
 
 export default function Main({ applicant, email, session_id, errors = {} }: MainProps) {
-    // fallback to empty object if applicant is undefined
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
     const initialForm: Applicant = {
         ...applicant,
         email: email || '',
     };
 
-    const { flash } = usePage<{ flash?: { success?: string } }>().props;
-
     return (
-        <div className="flex items-center justify-center bg-white p-10 md:bg-blue-900">
-            <Form className="w-full max-w-3xl rounded-lg border bg-white" action="/applicant/form" method="post" data={initialForm as any}>
-                {/* Hidden session ID */}
-                <input type="hidden" name="session_id" value={session_id} />
+        <div className="flex items-center justify-center bg-blue-900 p-10">
+            {!isSubmitted ? (
+                <Form
+                    className="w-full max-w-3xl rounded-lg border"
+                    action="/applicant/form"
+                    method="post"
+                    data={initialForm as any}
+                    resetOnSuccess
+                    onSuccess={() => setIsSubmitted(true)}
+                >
+                    <input type="hidden" name="session_id" value={session_id} />
 
-                {/* Step wizard with forms */}
-                <div className="min-h-[500px] rounded-sm bg-white p-6 text-black md:overflow-x-hidden md:overflow-y-auto md:p-20">
-                    <StepWizard>
-                        <PersonalInfoForm form={initialForm} errors={errors} />
-                        <JobPreference form={initialForm} errors={errors} />
-                        <WorkExperience form={initialForm} errors={errors} processing={false} />
-                    </StepWizard>
+                    <div className="min-h-[500px] rounded-sm bg-white p-6 text-black md:overflow-x-hidden md:overflow-y-auto md:p-20">
+                        <StepWizard>
+                            <PersonalInfoForm form={initialForm} errors={errors} />
+                            <JobPreference form={initialForm} errors={errors} />
+                            <WorkExperience form={initialForm} errors={errors} processing={false} />
+                        </StepWizard>
+                    </div>
+                </Form>
+            ) : (
+                //  success page
+                <div className="flex min-h-[550px] w-full max-w-2xl flex-col items-center justify-center rounded-lg bg-gray-300 p-10 shadow-lg">
+                    <img src="/images/success.png" alt="CheckIcon" className="h-20 w-20" />
+                    <p className="mt-5 text-center text-lg font-bold text-gray-800">Applicant profile submitted successfully!</p>
+
+                    <button
+                        className="mt-6 rounded-md bg-[#033284] px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#0242b3d2]"
+                        onClick={() => setIsSubmitted(false)}
+                    >
+                        <div className="flex items-center justify-center">
+                            <QrCode className="h-4 w-4" />
+                            <span className="ml-1">Generate QR</span>
+                        </div>
+                    </button>
                 </div>
-
-                {/* Flash success message */}
-                {flash?.success && <div className="mb-4 rounded bg-green-100 p-4 text-green-700">{flash.success}</div>}
-            </Form>
+            )}
         </div>
     );
 }
